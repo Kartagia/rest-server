fbb0f453d114127f70e4fe3aa422ec732f1854cb
Antti Kautiainen <antti@kautiainen.com>
2024-01-02 09:35:13 +0200

    feat: ValidationError
    modified:   rest-server-nextjs/src/errors.mjs
    - feat: ValidationError
      - An error representing a validation error.
    - feat: type ValidationErrorOptions
      - Options for validation error construction.

--- rest-server-nextjs/src/errors.mjs
+++ rest-server-nextjs/src/errors.mjs
@@ -5,6 +5,29 @@
  */
 
 /**
+ * @typedef {Object} ValidationErrorOptions
+ * @property {Error} [cause] The error causing the exception.
+ * @property {string} [target] The invalid resource or property name.
+ */
+
+/**
+ * A validation error indicates something was invalid.
+ */
+export class ValidationError extends Error {
+
+  /**
+   * Create a new validation error.
+   * @param {string} message 
+   * @param {ValidationErrorOptions} [options] 
+   */
+  constructor(message, options={}) {
+    super(message, {cause: options.cause});
+    this.name = this.constructor.name;
+    this.target = options.target;
+  }
+}
+
+/**
  * An error indicating an operation is not supported.
  */
 export class UnsupportedError extends Error {
